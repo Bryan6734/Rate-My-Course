e482f42ca03a4d8d5e31375b1584ba7497b9f886
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-04-15 18:30:50 -0400

    Added more fields to review model

--- backend/src/reviews/reviews.model.ts
+++ backend/src/reviews/reviews.model.ts
@@ -3,28 +3,45 @@ import { Document } from 'mongoose';
 
 export type ReviewDocument = Review & Document;
 
+// Every course will have a list of Ids which correspond to reviews
+// Every review will have the userId and the teacherId associated
+
 @Schema()
 export class Review {
-  @Prop()
-  humorRating: number;
+  @Prop({ required: true })
+  userId: string;
 
+  @Prop()
+  teacherId: string;
+
+  // How interesting/engaging was the content of the course?
+  @Prop()
+  content: number;
+
+  // How difficult was the course?
+  @Prop()
+  difficulty: number;
+
+  // How much work did the course require?
+  @Prop()
+  workload: number;
+
+  // What was the pacing of the course like?
+  @Prop()
+  pacing: number;
+
+  // How clear was the instructor of this course?
   @Prop()
   clarity: number;
 
+  // How organized was the content and/or teacher? 
   @Prop()
   organization: number;
 
-  @Prop()
-  grading: number;
-
-  @Prop()
-  teacherName: string;
-
+  // Please provide any additional comments about the course.
   @Prop()
   description: string;
 
-  @Prop()
-  id: string;
 }
 
 export const ReviewSchema = SchemaFactory.createForClass(Review);
