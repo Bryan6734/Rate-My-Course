56cdcd1647a1d16b9b4b5433769e667f6e817866
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-04-16 09:26:39 -0400

    Form dropdown with all fields Title, content, difficulty, workload, pacing, clarity, organization Text area for written review Submit, Reset, and char count

--- frontend/src/Components/CoursePage.js
+++ frontend/src/Components/CoursePage.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CoursePage.css";
 import ReviewDashboard from "./ReviewDashboard";
 
 function CoursePage({ course }) {
-  const desc = document.querySelector(".desc");
-  const descContents = document.querySelector(".desc-hidden");
-
-  const review = document.querySelector(".review-form");
-  const reviewContents = document.querySelector(".review-form-hidden");
+  const [inputs, setInputs] = useState({});
+  const [charCount, setCharCount] = useState(0);
+  const maxCharCount = 200;
 
   const showDescription = () => {
+    const desc = document.querySelector(".desc");
+    const descContents = desc.querySelectorAll("p");
+
     console.log("Animating desc");
+    console.log(desc.classList.contains("animate-in"));
 
     desc.classList.toggle("animate-in");
 
     // wait for animation to finish
     if (!desc.classList.contains("animate-in")) {
       setTimeout(() => {
-        descContents.classList.toggle("desc-hidden");
+        descContents.forEach((content) => {
+          content.classList.toggle("desc-hidden");
+        });
       }, 500);
     } else {
-      descContents.classList.toggle("desc-hidden");
+      descContents.forEach((content) => {
+        content.classList.toggle("desc-hidden");
+      });
     }
   };
 
   const showReviewForm = () => {
+    const review = document.querySelector(".review-form");
+    const reviewContents = review.querySelectorAll(":scope > *:not(h2)");
     console.log("Showing review form");
 
     if (!localStorage.getItem("user")) {
@@ -37,11 +45,33 @@ function CoursePage({ course }) {
     // wait for animation to finish
     if (!review.classList.contains("animate-in")) {
       setTimeout(() => {
-        reviewContents.classList.toggle("review-form-hidden");
+        reviewContents.forEach((content) => {
+          content.classList.toggle("review-form-hidden");
+        });
       }, 500);
     } else {
-      reviewContents.classList.toggle("review-form-hidden");
+      reviewContents.forEach((content) => {
+        content.classList.toggle("review-form-hidden");
+      });
     }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitting");
+
+    const x = JSON.stringify(inputs);
+    console.log(x);
   };
 
   return (
@@ -63,8 +93,68 @@ function CoursePage({ course }) {
         <div className="review-form">
           <h2 onClick={() => showReviewForm()}>Submit Your Review</h2>
 
-          <form action="" className="review-form-hidden">
-            <input type="text" name="title" id="title" placeholder="Title" />
+          <form onSubmit={handleSubmit} className="review-form-hidden">
+            <input type="text" name="title" id="title" value={inputs.title || ""} onChange={handleChange} placeholder="Title" />
+
+            <div className="sliders">
+              <div className="slider">
+                <label htmlFor="content">Content</label>
+
+                <input type="range" name="content" id="content" value={inputs.content || ""} onChange={handleChange} />
+              </div>
+
+              <div className="slider">
+                <label htmlFor="difficulty">Difficulty</label>
+                <input type="range" name="difficulty" id="difficulty" value={inputs.difficulty || ""} onChange={handleChange} />
+              </div>
+
+              <div className="slider">
+                <label htmlFor="workload">Workload</label>
+                <input type="range" name="workload" id="workload" value={inputs.workload || ""} onChange={handleChange} />
+              </div>
+
+              <div className="slider">
+                <label htmlFor="pacing">Pacing</label>
+                <input type="range" name="pacing" id="pacing" value={inputs.pacing || ""} onChange={handleChange} />
+              </div>
+
+              <div className="slider">
+                <label htmlFor="clarity">Clarity</label>
+                <input type="range" name="clarity" id="clarity" value={inputs.clarity || ""} onChange={handleChange} />
+              </div>
+
+              <div className="slider">
+                <label htmlFor="organization">Organization</label>
+                <input type="range" name="organization" id="organization" value={inputs.organization || ""} onChange={handleChange} />
+              </div>
+            </div>
+
+            <textarea
+              name="description"
+              id="description"
+              placeholder="Write a little bit about your experience in this course."
+              maxLength={maxCharCount}
+              value={inputs.description || ""}
+              onChange={(e) => {
+                handleChange(e);
+                setCharCount(e.target.value.length);
+              }}
+            ></textarea>
+
+            <div className="review-final-row">
+              <div className="buttons">
+                <button type="submit">Submit</button>
+                <button type="reset" onClick={(e) => {
+                  e.preventDefault();
+                  setInputs({});
+                  setCharCount(0);
+                }}>Reset</button>
+              </div>
+
+              <p className="char-count">
+                {charCount}/{maxCharCount}
+              </p>
+            </div>
           </form>
         </div>
       </div>
