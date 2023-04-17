0a3b725c47a38054b6bb341e68129e81b131d78d
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-04-17 14:55:45 -0400

    New account page Displays user profile and all a user's reviews Moved logout button here Still have issues w/ dynamic resizing and API redundancy

--- frontend/src/Components/UserProfile.js
+++ frontend/src/Components/UserProfile.js
@@ -1,19 +1,68 @@
 import React from "react";
 import "./UserProfile.css";
+// import ReviewCard from "./ReviewCard";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ReviewCard from "./ReviewCard";
 
 function UserProfile() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    graduationYear: "",
+    picture: "",
+  });
+  const [reviews, setReviews] = useState([]);
+  const navigate = useNavigate();
+
+  const handleClick = (event, courseName) => {
+    navigate("/" + courseName);
+  };
+
+  const logout = () => {
+    navigate("/");
+    console.log("Button clicked");
+    localStorage.removeItem("user");
+    // refresh
+    setUser({
+      firstName: "",
+      lastName: "",
+      email: "",
+      graduationYear: "",
+      picture: "",
+    });
+    window.location.reload();
+  };
 
   useEffect(() => {
     const googleId = JSON.parse(localStorage.getItem("user")).googleId;
 
-    fetch("http://127.0.0.1:8000/users/googleId/" + googleId)
-      .then((res) => res.json())
-      .then((data) => {
-        setUser(data);
-        console.log("API data", data);
+    // API: user w/ googleId -> reviews -> courses
+    const getUserReviews = async () => {
+      const userDBResponse = await fetch("http://127.0.0.1:8000/users/googleId/" + googleId);
+      const userData = await userDBResponse.json();
+      setUser(userData);
+
+      const reviewsDBResponse = await fetch("http://127.0.0.1:8000/reviews/user/" + userData.googleId);
+
+      const reviewsData = await reviewsDBResponse.json();
+      const courseIds = [...new Set(reviewsData.map((review) => review.courseId))];
+
+      const courseDBResponse = await fetch("http://127.0.0.1:8000/courses/ids/multiple?ids=" + courseIds.join(","));
+      const courseData = await courseDBResponse.json();
+
+      // merge reviews and courses
+      const reviewsWithCourses = reviewsData.map((review) => {
+        const course = courseData.find((course) => course._id === review.courseId);
+
+        return { ...review, course };
       });
+
+      setReviews(reviewsWithCourses);
+    };
+
+    getUserReviews();
   }, []);
 
   return (
@@ -24,10 +73,32 @@ function UserProfile() {
       <div className="profile-card">
         <img src={user.picture} alt="User" />
         <div className="info">
-          <h2>{user.name}</h2>
-          <div className="details">
+          <div className="info-text">
+            <h2>{user.firstName + " " + user.lastName}</h2>
+            <p>{"Class of " + user.graduationYear}</p>
             <p>{user.email}</p>
           </div>
+
+          <div className="buttons">
+            <button className="logout btn" onClick={() => logout()}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+      <div className="reviews">
+        <h1>Reviews</h1>
+
+        <div className="review-list">
+          {reviews.map((review, index) => {
+            return (
+              <div onClick={(event) => handleClick(event, review.course.name)} key={index}>
+                <ReviewCard review={review} details={review.course.name}></ReviewCard>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
