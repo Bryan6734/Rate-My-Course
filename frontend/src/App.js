731596fc728c0a19ac04c9073a080b3552cc4075
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-04-25 17:59:51 -0400

    Added more documentation for course fetching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [courses, setCourses] = useState([]);
 
+  // Fetches all courses from the database
   useEffect(() => {
     fetch("http://localhost:8000/courses")
       .then((res) => res.json())
