b54c652fb1df28a279a83337cc72838cf5286edb
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-04-26 16:53:54 -0400

    Added about me to mobile menu bar Fixed mobile issues on menu bar (made scrollable)

--- frontend/src/Components/Menu.js
+++ frontend/src/Components/Menu.js
@@ -22,15 +22,16 @@ function Menu() {
 
   return (
     <div className="menu hide">
-      <img className="close" src={menuClose} onClick={closeMenu} alt="" />
       <ul className="menu-links">
+      <img className="close" src={menuClose} onClick={closeMenu} alt="" />
         <hr />
         <li onClick={() => handleClick("home")}>Home</li>
-        <li>{localStorage.getItem("user") ? <AccountButton user={user}menuRender={true}></AccountButton> : <LoginButton></LoginButton>}</li>
+        <li onClick={() => handleClick("about-us")}>About Us</li>
         <li onClick={() => {
           navigate("/courses")
         }}>Courses</li>
-        <li onClick={() => handleClick("about-us")}>About Us</li>
+        <li>{localStorage.getItem("user") ? <AccountButton user={user}menuRender={true}></AccountButton> : <LoginButton></LoginButton>}</li>
+        <hr />
         <li>Mathematics</li>
         <li>Computer Science</li>
         <li>Sciences</li>
